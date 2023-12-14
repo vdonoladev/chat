5873917b88a46e4a9e323706411eb6aacf384bbc
vdonoladev <victordonolaf@gmail.com>
2023-12-14 18:02:41 -0300

    changing server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
   login.style.display = "none"
   chat.style.display = "flex"
 
-  websocket = new WebSocket("ws://localhost:8080")
+  websocket = new WebSocket("wss://chat-backend-5yyx.onrender.com")
   websocket.onmessage = processMessage
 }
 
